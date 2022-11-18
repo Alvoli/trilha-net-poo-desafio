999c6216bd883dfe14fe34a7c50d68290f227fc5
Alvoli <alvolioficial@gmail.com>
2022-11-17 23:21:33 -0400

    TERMINEEEEEEEEEEEEEEEEEEEEEI

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,22 @@
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
-    {
-        public string Numero { get; set; }
+    {   
+        public string Numero {get; set;}
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo;
 
-        public Smartphone(string numero)
+        private string IMEI;
+
+        private int Memoria;
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Memoria = memoria;
+            Modelo = modelo;
+            IMEI = imei;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia 8.1:");
+Nokia N = new Nokia("938571534","Nokia 8.1","1234567890", 128);
+N.Ligar();
+N.ReceberLigacao();
+N.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone XS MAX:");
+Iphone I = new Iphone("998765723", "Iphone XS MAX", "987654431", 256);
+I.Ligar();
+I.ReceberLigacao();
+I.InstalarAplicativo("Telegram");
